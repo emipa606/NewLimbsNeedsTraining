79789b085df84e9b663afee8fb0791e7bbd591c3
emipa606 <emil.offermo@litet.net>
2021-03-29 11:56:13 +0200

    Minor correction for the spawn-check

--- Source/NewLimbsNeedsTraining/PawnGenerator_GeneratePawn.cs
+++ Source/NewLimbsNeedsTraining/PawnGenerator_GeneratePawn.cs
@@ -11,6 +11,11 @@ namespace NewLimbsNeedsTraining
         {
             foreach (var hediffAddedPart in __result.health.hediffSet.GetHediffs<Hediff_AddedPart>())
             {
+                if (hediffAddedPart.ageTicks >= 1)
+                {
+                    continue;
+                }
+
                 hediffAddedPart.ageTicks = Rand.Range(NewLimbsNeedsTraining.ticksUntilDone,
                     NewLimbsNeedsTraining.ticksUntilDone * 2);
             }
