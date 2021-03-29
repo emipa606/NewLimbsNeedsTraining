07a08e3aeba1ba994c2cf0b9358ab4d5368a0a2b
emipa606 <emil.offermo@litet.net>
2021-03-29 11:05:48 +0200

    Should now not affect pawns that spawn in from outside

--- Source/NewLimbsNeedsTraining/HediffWithComps_CalculatePartEfficiency.cs
+++ Source/NewLimbsNeedsTraining/HediffWithComps_CalculatePartEfficiency.cs
@@ -10,39 +10,49 @@ namespace NewLimbsNeedsTraining
         typeof(bool), typeof(List<PawnCapacityUtility.CapacityImpactor>))]
     public class HediffWithComps_CalculatePartEfficiency
     {
+        private static readonly int oneDayInTicks = 60000;
+
+        private static readonly int ticksUntilDone = LoadedModManager.GetMod<NewLimbsNeedsTrainingMod>()
+            .GetSettings<NewLimbsNeedsTrainingSettings>().DaysUntilRecovery * oneDayInTicks;
+
         private static float getOffset(int age, float incomingEfficency)
         {
             // 60000 ticks is one day
             // 900000 ticks is one quandrum (15 days)
-            var day = 60000;
-            var daysUntilFullEfficency = LoadedModManager.GetMod<NewLimbsNeedsTrainingMod>()
-                .GetSettings<NewLimbsNeedsTrainingSettings>().DaysUntilRecovery;
-            var factor = (float) age / (daysUntilFullEfficency * day);
+            var factor = (float) age / ticksUntilDone;
             return Math.Min(incomingEfficency, incomingEfficency * factor);
         }
 
         [HarmonyPostfix]
         public static void Postfix(HediffSet diffSet, BodyPartRecord part, ref float __result)
         {
+            if (!diffSet.pawn.IsColonist && !diffSet.pawn.IsPrisonerOfColony)
+            {
+                // Log.Message("Probably shouldnt affect raiders and travelers");
+                return;
+            }
+
             if (part.depth != BodyPartDepth.Outside)
             {
-                //Log.Message("Dont affect things we cannot see");
+                // Log.Message("Dont affect things we cannot see");
                 return;
             }
 
             if (!diffSet.HasDirectlyAddedPartFor(part))
             {
-                //Log.Message($"No directly added part for {part.def.defName}");
+                // Log.Message($"No directly added part for {part.def.defName}");
                 return;
             }
 
             var hediffs = diffSet.GetHediffs<Hediff_AddedPart>();
             var hediff_AddedPart = hediffs.First(x => x.Part == part);
-            var partEfficencyOffset = getOffset(hediff_AddedPart.ageTicks, __result);
-            //Log.Message(
-            //    $"{hediff_AddedPart.def.defName}: efficency set to {partEfficencyOffset}, down from {__result}");
+            if (hediff_AddedPart.ageTicks > ticksUntilDone)
+            {
+                // Log.Message($"Hediff trained {part.def.defName}");
+                return;
+            }
 
-            __result = partEfficencyOffset;
+            __result = getOffset(hediff_AddedPart.ageTicks, __result);
         }
     }
 }
